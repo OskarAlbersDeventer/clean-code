9ce508091d1392bcaf1cb5b2d1c6ecb2bb626119
Oskar Albers <oskaralbers@gmail.com>
2019-04-17 13:16:27 +0200

    Split the main method to reduce the complexity and increase readability

--- src/main/java/com/wunderman/codequality/MainController.java
+++ src/main/java/com/wunderman/codequality/MainController.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -22,14 +21,43 @@ import org.springframework.web.bind.annotation.RestController;
 import com.wunderman.codequality.model.Category;
 
 
-
 @RestController
 public class MainController {
+
     @GetMapping("categories")
     public List<Category> getCategories() {
         System.out.println("Starting getCategories");
         List<Category> categoryList = new ArrayList<>();
+
+        String categoryJsonString = getCategoriesFromIntershop();
+        JSONObject categoryJson = parseTheJsonResult(categoryJsonString);
+        if(categoryJson != null) {
+            JSONArray categoryJsonArray = (JSONArray) categoryJson.get("elements");
+            System.out.println(categoryJsonArray);
+            for (int i = 0; i < categoryJsonArray.size(); i++) {
+                JSONObject category = (JSONObject) categoryJsonArray.get(i);
+                System.out.println(category);
+                categoryList.add(convertIntershopCategory(category));
+            }
+        }
+        return categoryList;
+    }
+
+    private static JSONObject parseTheJsonResult(String inputJson) {
+        System.out.println(inputJson);
+        JSONObject jsonObject = null;
+        JSONParser jsonParser = new JSONParser();
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(inputJson);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return jsonObject;
+    }
+
+    private static String getCategoriesFromIntershop() {
         StringBuilder responseString = new StringBuilder();
+
         try {
             HttpClient client = HttpClientBuilder.create().build();
             HttpGet request = new HttpGet("https://jxdemoserver6.intershop.de/INTERSHOP/rest/WFS/inSPIRED-inTRONICS-Site/-/categories");
@@ -45,38 +73,26 @@ public class MainController {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        if (responseString.length() > 0) {
-            try {
-                JSONParser jsonParser = new JSONParser();
-                JSONObject tomJsonObject = (JSONObject) jsonParser.parse(responseString.toString());
-                System.out.println(tomJsonObject);
-                JSONArray tomJsonArray = (JSONArray) tomJsonObject.get("elements");
-                System.out.println(tomJsonArray);
-                for (int i = 0; i < tomJsonArray.size(); i++) {
-                    tomJsonObject = (JSONObject) tomJsonArray.get(i);
-                    System.out.println(tomJsonObject);
-                    Category c1 = new Category();
-                    if((boolean)tomJsonObject.get("hasOnlineSubCategories")){
-                        c1.setSubcategoriesAvailable(true);
-                    }
-                    c1.setDescription((String)tomJsonObject.get("description"));
-                    c1.setOnline((boolean)tomJsonObject.get("hasOnlineProducts"));
 
-                    c1.setId((String) tomJsonObject.get("id"));
-                    c1.setName((String) tomJsonObject.get("name"));
-                    if (tomJsonObject.get("online").equals("1")) {
-                        c1.setOnline(true);
-                    }
-                    c1.setUri((String) tomJsonObject.get("uri"));
-                    c1.setSpecial((boolean) tomJsonObject.get("hasOnlineProducts") && !(boolean) tomJsonObject.get("hasOnlineSubCategories"));
-                    categoryList.add(c1);
-                }
-            } catch (ParseException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        return responseString.toString();
+    }
+
+    private static Category convertIntershopCategory(JSONObject categoryJsonObject) {
+        Category category = new Category();
+        if ((boolean) categoryJsonObject.get("hasOnlineSubCategories")) {
+            category.setSubcategoriesAvailable(true);
         }
-        return categoryList;
+        category.setDescription((String) categoryJsonObject.get("description"));
+        category.setOnline((boolean) categoryJsonObject.get("hasOnlineProducts"));
+
+        category.setId((String) categoryJsonObject.get("id"));
+        category.setName((String) categoryJsonObject.get("name"));
+        if (categoryJsonObject.get("online").equals("1")) {
+            category.setOnline(true);
+        }
+        category.setUri((String) categoryJsonObject.get("uri"));
+        category.setSpecial((boolean) categoryJsonObject.get("hasOnlineProducts") && !(boolean) categoryJsonObject.get("hasOnlineSubCategories"));
+
+        return category;
     }
 }
