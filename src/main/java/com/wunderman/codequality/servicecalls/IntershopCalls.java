c812aca382438743df61b579a1661d88514599f4
Oskar Albers <oskar.albers@topicus.nl>
2025-10-22 09:34:59 +0200

    logback config aangepast

--- src/main/java/com/wunderman/codequality/servicecalls/IntershopCalls.java
+++ src/main/java/com/wunderman/codequality/servicecalls/IntershopCalls.java
@@ -1,18 +1,15 @@
 package com.wunderman.codequality.servicecalls;
 
 import com.wunderman.codequality.model.intershop.CategoryList;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.MediaType;
+import org.springframework.web.client.RestClient;
 
 public class IntershopCalls implements IServiceCalls{
 
-    private Client client = ClientBuilder.newClient();
-
     @Override
     public CategoryList getCategories(String hostUrl) {
-       CategoryList result =  client.target(hostUrl).request(MediaType.APPLICATION_JSON).get(CategoryList.class);
+        RestClient restClient = RestClient.builder().baseUrl(hostUrl).build();
+        CategoryList result = restClient.get().retrieve().body(CategoryList.class);
+
        return  result;
     }
 }
